dcf00e6f4b11aa0ee28f8cc58b97e93589e6e150
Régis Júnior <regisamxjr@gmail.com>
2024-12-25 13:23:42 -0300

    adicionar botão de tradução para inglês

---
+++ script.js
@@ -0,0 +1,24 @@
+function translateSite(language) {
+    const translations = {
+        pt: {
+            habilidades: "Minhas Habilidades",
+            sobre: "Sobre Mim",
+            projetos: "Meus Projetos",
+            contrate: "Me contrate",
+            tela1: "DESENVOLVEDOR DE SOFTWARE",
+ 
+        },
+        en: {
+            habilidades: "My Skills",
+            sobre: "About Me",
+            projetos: "My Projects",
+            contrate: "Hire Me",
+            tela1: "SOFTWARE DEVELOPER",
+         
+        }
+    };
+
+    document.querySelector("h1").textContent = "Régis Xavier";
+    document.querySelectoral.write().start++; 
+
+    }
